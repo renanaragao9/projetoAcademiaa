d9dd5d5b4e6e4f9d4f97a288281979751f666b11
renanaragao9 <renanaragao159@gmail.com>
2024-03-31 23:11:03 -0300

    concluindo email

--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -75,11 +75,11 @@ class ResetPassword extends Notification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(Lang::get('Notificação de redefinição de senha'))
+            ->line(Lang::get('Você está recebendo este e-mail porque recebemos uma solicitação de redefinição de senha da sua conta'))
+            ->action(Lang::get('Redefinir Senha'), $url)
+            ->line(Lang::get('Este link de redefinição de senha expirará em :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Se você não solicitou uma redefinição de senha, nenhuma ação adicional será necessária.'));
     }
 
     /**
